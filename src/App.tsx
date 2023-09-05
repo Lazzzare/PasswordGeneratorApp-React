a8357e1f4800b25654c4af3eb35e15f383af0612
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-05 19:13:05 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Input from "./Input";
+import PasswordGenerator from "./PasswordGenerator";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen bg-bgColor flex flex-col">
+      <Input title="Password Generator" />
+      <PasswordGenerator />
+    </div>
+  );
 };
 
 export default App;
