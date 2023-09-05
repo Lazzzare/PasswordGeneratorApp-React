94f58f87f6f6da31cebe6583d76078656218ad3d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-05 18:19:13 +0400

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlack: "#24232C",
+        grey: "#817D92",
+        white: "#E6E5EA",
+        darkBlack: "#18171F",
+        green: "#A4FFAF",
+        red: "#F64A4A",
+        orange: "#FB7C58",
+        yellow: "#F8CD65",
+      },
+    },
   },
   plugins: [],
 };
