e0e3684f2c37ee9b8565e4335eee73098f58350a
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-09 18:18:39 +0400

    Update PasswordGenerator.tsx

--- src/passwordGenerator/PasswordGenerator.tsx
+++ src/passwordGenerator/PasswordGenerator.tsx
@@ -1,3 +1,4 @@
+import { useState, ChangeEvent } from "react";
 import Strength from "./Strength";
 import GeneratorButton from "./GeneratorButton";
 import CheckBox from "./CheckBox";
@@ -6,21 +7,53 @@ import CharacterAmount from "./CharacterAmount";
 interface props {
   generateRandomCharacters: () => void;
   rangeValue: number;
-  handleInputChange: (e: any) => void;
+  handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
+
+const checkBoxArray = [
+  {
+    id: 1,
+    text: "Include Uppercase Letters",
+  },
+  {
+    id: 2,
+    text: "Include Lowercase Letters",
+  },
+  {
+    id: 3,
+    text: "Include Numbers",
+  },
+  {
+    id: 4,
+    text: "Include Symbols",
+  },
+];
 
 const PasswordGenerator = ({
   generateRandomCharacters,
   rangeValue,
   handleInputChange,
 }: props) => {
+  const [checkboxStates, setCheckboxStates] = useState(
+    checkBoxArray.map(() => false)
+  );
+
+  const handleCheckboxClick = (index: number) => {
+    const updatedStates = [...checkboxStates];
+    updatedStates[index] = !updatedStates[index];
+    setCheckboxStates(updatedStates);
+  };
   return (
     <div className="bg-lightBlack p-4 md:pt-6 md:px-8 md:pb-8 mt-4 w-[343px] md:w-[540px] mx-auto">
       <CharacterAmount
         rangeValue={rangeValue}
         handleInputChange={handleInputChange}
       />
-      <CheckBox />
+      <CheckBox
+        handleCheckboxClick={handleCheckboxClick}
+        checkBoxArray={checkBoxArray}
+        checkboxStates={checkboxStates}
+      />
       <Strength />
       <GeneratorButton generateRandomCharacters={generateRandomCharacters} />
     </div>
