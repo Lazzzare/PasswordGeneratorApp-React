751cbf6e75a6f8890ea402ae04babed2e99accef
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-16 18:20:00 +0400

    Update PasswordGenerator.tsx

--- src/passwordGenerator/PasswordGenerator.tsx
+++ src/passwordGenerator/PasswordGenerator.tsx
@@ -1,45 +1,27 @@
-import React, { useState } from "react";
+import { ChangeEvent } from "react";
 import Strength from "./Strength";
 import GeneratorButton from "./GeneratorButton";
 import CheckBox from "./CheckBox";
 import CharacterAmount from "./CharacterAmount";
-import { ChangeEvent } from "react";
 
-interface PasswordGeneratorProps {
+interface props {
   generateRandomCharacters: () => void;
   rangeValue: number;
-  handleInputChange: (event: ChangeEvent<HTMLInputElement>) => void; // Update type here
+  handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
   handleCheckboxClick: (index: number) => void;
   checkBoxArray: { id: number; text: string }[];
   checkboxStates: boolean[];
 }
 
-const PasswordGenerator: React.FC<PasswordGeneratorProps> = () => {
-  const [checkboxStates, setCheckboxStates] = useState<boolean[]>([
-    false,
-    false,
-    false,
-    false,
-  ]);
-  const [rangeValue, setRangeValue] = useState<number>(0);
-  const [count, setCount] = useState<number>(0);
-
-  const handleCheckboxClick = (index: number) => {
-    const updatedCheckboxStates = [...checkboxStates];
-    updatedCheckboxStates[index] = !updatedCheckboxStates[index];
-    setCheckboxStates(updatedCheckboxStates);
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newValue = parseInt(event.target.value, 10);
-    newValue = isNaN(newValue) ? 0 : Math.min(20, Math.max(0, newValue));
-    setRangeValue(newValue);
-  };
-
-  const generateRandomCharacters = () => {
-    const trueCount = checkboxStates.filter((item) => item).length;
-    setCount(trueCount);
-  };
+const PasswordGenerator = ({
+  generateRandomCharacters,
+  rangeValue,
+  handleInputChange,
+  handleCheckboxClick,
+  checkBoxArray,
+  checkboxStates,
+}: props) => {
+  const trueCount = checkboxStates.filter((item) => item).length;
 
   return (
     <div className="bg-lightBlack p-4 md:pt-6 md:px-8 md:pb-8 mt-4 w-[343px] md:w-[540px] mx-auto">
@@ -49,20 +31,16 @@ const PasswordGenerator: React.FC<PasswordGeneratorProps> = () => {
       />
       <CheckBox
         handleCheckboxClick={handleCheckboxClick}
-        checkBoxArray={[
-          { id: 0, text: "Uppercase Letters" },
-          { id: 1, text: "Lowercase Letters" },
-          { id: 2, text: "Numbers" },
-          { id: 3, text: "Symbols" },
-        ]}
+        checkBoxArray={checkBoxArray}
         checkboxStates={checkboxStates}
       />
-      <Strength
-        checkboxStates={checkboxStates}
+      <Strength count={trueCount} />
+      <GeneratorButton
         generateRandomCharacters={generateRandomCharacters}
-        count={count}
+        updateStrength={function (): void {
+          throw new Error("Function not implemented.");
+        }}
       />
-      <GeneratorButton generateRandomCharacters={generateRandomCharacters} />
     </div>
   );
 };
